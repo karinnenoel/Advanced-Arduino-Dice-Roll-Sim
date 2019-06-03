ae5d78468f92536871356daebd4ac257ae4d9108
Karinne App <k.app@students.chapelfield.org>
2019-06-03 12:41:13 -0400

    Dinfine pins that will be connected to LEDs

---
+++ Advanced-Arduino-Dice-Roll-Sim/Advanced-Arduino-Dice-Roll-Sim.ino
@@ -0,0 +1,35 @@
+int leftTop = 2;
+int leftMiddle = 3;
+int leftBottom = 4;
+int middleMiddle = 5;
+int rightTop = 6;
+int rightMiddle = 7;
+int rightBottom = 8;
+
+int ledPins[] = {
+  leftTop,
+  leftMiddle,
+  leftBottom,
+  middleMiddle,
+  rightTop,
+  rightMiddle,
+  rightBottom,
+};
+
+int numLedPins = 7;
+
+
+
+
+
+
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
